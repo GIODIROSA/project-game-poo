2f086713601c28b1525742d9b6c32bd5d62e1dd3
giovanni di rosa <gdirosa@flagare.cl>
2024-12-08 11:00:15 -0300

    [ADD] se agrega los getter y setter en personaje

--- DTO/jugador.py
+++ DTO/jugador.py
@@ -5,9 +5,12 @@ class Jugador(Personaje):
     __raza = ""
     __experiencia = 0
     
-    def __init__(self, raza, experiencia):
+    def __init__(self, raza, experiencia, nombre, nivel, salud):
         self.__raza = raza
         self.__experiencia = experiencia
+        self.setNombre(nombre)
+        self.setNivel(nivel)
+        self.setSalud(salud)
 
 
     def funcionalidad(self):
--- DTO/personaje.py
+++ DTO/personaje.py
@@ -5,6 +5,24 @@ class Personaje:
      __nivel = 1
      __salud= 100
 
+     def getNombre(self):
+          return self.__nombre
+     
+     def getNivel(self):
+          return self.__nivel
+     
+     def getSalud(self):
+          return self.__salud
+     
+     def setNombre(self, nombre):
+          self.__nombre = nombre
+
+     def setNivel(self, nivel):
+          self.__nivel = nivel
+
+     def setSalud(self, salud):
+          self.__salud = salud
+
 
      def atacar(self):
           pass
