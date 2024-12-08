6e2d716a6bb2d1226315a5547e2e0a001e3b4861
giovanni di rosa <gdirosa@flagare.cl>
2024-12-08 09:26:24 -0300

    [CHG] se hace cambio de nombre de diagrama y se agrega atributos a pocion

--- DTO/inventario/pocion.py
+++ DTO/inventario/pocion.py
@@ -1,10 +1,10 @@
 
 class Pocion():
-    tipo = ""
-    defensa= 0
+    nombre = ""
+    cura= 0
 
 
-    def __init__(self, tipo, defensa):
-        self.tipo = tipo
-        self.defensa = defensa
+    def __init__(self, nombre, cura):
+        self.nombre = nombre
+        self.cura = cura
   
