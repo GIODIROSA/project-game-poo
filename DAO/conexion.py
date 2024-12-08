4ca52b1fe2b6b9a1f2a9a40413f6cfabaa86fc74
giovanni di rosa <gdirosa@flagare.cl>
2024-12-08 11:08:44 -0300

    [ADD] se habilita conexion con mysql

---
+++ DAO/conexion.py
@@ -0,0 +1,36 @@
+import pymysql
+import sys
+import io
+
+
+# Configuración de conexión
+host = "localhost"        
+user = "dragons"       
+password = "okidoki" 
+database = "dungeon_and_dragons" 
+
+try:
+    # Estableciendo conexión
+    conexion = pymysql.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database
+    )
+    print("¡Conexión exitosa!")
+
+
+    with conexion.cursor() as cursor:
+    
+        cursor.execute("SELECT VERSION();")
+        version = cursor.fetchone()
+        print(f"Versión de MySQL: {version[0]}")
+
+except pymysql.MySQLError as e:
+    print("Ocurrió un error al conectar a MySQL:", e)
+
+finally:
+ 
+    if 'conexion' in locals() and conexion.open:
+        conexion.close()
+        print("Conexión cerrada.")
